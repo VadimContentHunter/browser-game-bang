618955259ea974798619c8e908e3c6c69a6d41a8
VadimContentHunter <project.k.vadim@gmail.com>
2025-04-29 14:52:35 +0300

    feat(types): add session type declarations and include in tsconfig

---
+++ public/types/session.d.ts
@@ -0,0 +1,10 @@
+// types/session.d.ts
+
+// declare module 'express-session' {
+//     interface SessionData {
+//         user?: {
+//             id: string;
+//             username: string;
+//         };
+//     }
+// }
